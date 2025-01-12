77e336b519c08de50c9e15ae68204e63a7bef579
ashutoshdx <ashutosh.dubey@xemailverify.com>
2025-01-12 23:45:19 +0530

    created struct, implemented fn init created debug function which can only be called by struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-fn main() {
-    let ans = fib(4);
-    print!("{}", ans);
-    // println!("Hello, world!");
-}
+// fn main() {
+//     let ans = fib(4);
+//     print!("{}", ans);
+//     // println!("Hello, world!");
+// }
 // 0 1 1 2 3 5
 // fn is_even(num: i32) -> bool {
 //     if num % 2 == 0 {
@@ -11,22 +11,46 @@ fn main() {
 //     return false;
 // }
 
-fn fib(num: u32) -> u32 {
-    let mut first_num: u32 = 0;
-    let mut second_num: u32 = 1;
-    let mut count = 2;
-    if num == 0 {
-        return first_num;
+// fn fib(num: u32) -> u32 {
+//     let mut first_num: u32 = 0;
+//     let mut second_num: u32 = 1;
+//     let mut count = 2;
+//     if num == 0 {
+//         return first_num;
+//     }
+//     if num == 1 {
+//         return second_num;
+//     }
+//     // let mut new_num: u32;
+//     while count <= num {
+//         let new_num = first_num + second_num;
+//         first_num = second_num;
+//         second_num = new_num;
+//         count = count + 1;
+//     }
+//     return second_num;
+// }
+
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
     }
-    if num == 1 {
-        return second_num;
+
+    fn debug() -> u32 {
+        return 1;
     }
-    // let mut new_num: u32;
-    while count <= num {
-        let new_num = first_num + second_num;
-        first_num = second_num;
-        second_num = new_num;
-        count = count + 1;
-    }
-    return second_num;
+}
+
+fn main() {
+    let rect = Rect {
+        width: 10,
+        height: 20,
+    };
+    print!("Area of the rectangle is {}\n", rect.area());
+    print!("Debug functin {}", Rect::debug());
 }
