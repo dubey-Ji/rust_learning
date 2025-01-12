5750a57d25e73e224ea6ce88c8b10e7d33ef5548
ashutoshdx <ashutosh.dubey@xemailverify.com>
2025-01-12 23:20:30 +0530

    created fibonaaci series

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 fn main() {
-    let ans = is_even(11);
+    let ans = fib(4);
     print!("{}", ans);
     // println!("Hello, world!");
 }
+// 0 1 1 2 3 5
+// fn is_even(num: i32) -> bool {
+//     if num % 2 == 0 {
+//         return true;
+//     }
+//     return false;
+// }
 
-fn is_even(num: i32) -> bool {
-    if num % 2 == 0 {
-        return true;
+fn fib(num: u32) -> u32 {
+    let mut first_num: u32 = 0;
+    let mut second_num: u32 = 1;
+    let mut count = 2;
+    if num == 0 {
+        return first_num;
     }
-    return false;
+    if num == 1 {
+        return second_num;
+    }
+    // let mut new_num: u32;
+    while count <= num {
+        let new_num = first_num + second_num;
+        first_num = second_num;
+        second_num = new_num;
+        count = count + 1;
+    }
+    return second_num;
 }
